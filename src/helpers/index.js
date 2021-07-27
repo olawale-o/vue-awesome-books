edabc2856125debed567ee30edfa236ad82dda4c
olawale-o <omoogunolawale@gmail.com>
2021-07-27 11:07:35 +0100

    Add numberSuffix function in helpers directory

---
+++ src/helpers/index.js
@@ -0,0 +1,14 @@
+const numberSuffix = (num) => {
+  if (num >= 11 && num <= 13) return 'th';
+
+  const lastDigit = num.toString().slice(-1);
+
+  switch (lastDigit) {
+    case '1': return 'st';
+    case '2': return 'nd';
+    case '3': return 'rd';
+    default: return 'th';
+  }
+};
+
+export default numberSuffix;
