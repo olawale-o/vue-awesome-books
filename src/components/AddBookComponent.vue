c724d93aa36f6ccf8e0aa3afee2cfa59cebabc2b
olawale-o <omoogunolawale@gmail.com>
2021-08-12 14:11:02 +0100

    Add AddBookComponent.vue

---
+++ src/components/AddBookComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="form" id="form">
+    <h2>Add a new book</h2>
+    <form class="add-book-form" id="add-book-form" @submit.prevent="onFormSubmit">
+      <div>
+        <input type="text" name="title" v-model="book.title" placeholder="Title" required>
+        <p id="title-error"></p>
+      </div>
+      <div>
+        <input type="text" name="author" v-model="book.author" placeholder="Author" required>
+        <p id="author-error"></p>
+      </div>
+        <input type="submit" value="Add" id="add-book-button">
+    </form>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+  name: "AddBookComponent",
+  emits: ["addBook"],
+  setup(props, { emit }) {
+    const book = reactive({
+      id: '',
+      author: '',
+      title: ''
+    });
+
+    const onFormSubmit = () => {
+      emit('addBook', { author: book.author, title: book.title });
+      book.author = '';
+      book.title = '';
+    } 
+    return {
+      book, onFormSubmit,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
